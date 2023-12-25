b4f87d3be2329886aa16618be5cd79d588daefab
Harshita Nimje <harshunimje22@gmail.com>
2023-12-25 11:11:54 +0530

    docs

---
+++ src/Components/Foreground.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Card from './Card'
+import { useRef,useState } from 'react';
+
+function Foreground() {
+    const ref = useRef(null);
+  const data=[
+    {
+      desc:"This is the background colour of the card that will displayed",
+      filesize:".9mb",
+      close:true,
+      tag: {isOpen: true, tagTitle: "Uplaod", tagColor:"green"}
+    },
+    {
+      desc:"This is the background colour of the card that will displayed",
+      filesize:".9mb",
+      close:true,
+      tag: {isOpen: true, tagTitle: "Download Now", tagColor:"blue"}
+    },
+    {
+      desc:"This is the background colour of the card that will displayed",
+      filesize:".9mb",
+      close:true,
+      tag: {isOpen: false, tagTitle: "Uplaod", tagColor:"green"}
+    },
+  ];
+  return (
+    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item, index)=>(
+          <Card data={item} reference={ref}/>
+        ))}
+    </div>
+  )
+}
+
+export default Foreground
